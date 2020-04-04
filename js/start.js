1f80afaeaa7ffc3c4e6ae367dc8089e10c3ad577
Dmitry Ageev <dimichageev@icloud.com>
2020-04-04 23:45:35 +0300

    Нарисовал тень

--- js/start.js
+++ js/start.js
@@ -1,6 +1,12 @@
 'use strict';
+var renderCloud = function() {
 
-window.renderStatistics = function(ctx) {
+};
+
+window.renderStatistics = function(ctx, names, times) {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(110, 20, 420, 270);
+
   ctx.fillStyle = '#fff';
-  ctx.fillRect(100, 50, 500, 200);
+  ctx.fillRect(100, 10, 420, 270);
 };
