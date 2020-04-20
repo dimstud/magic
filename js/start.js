ebd9b6756e44a02c8bae7186e111728efcbef0ef
Dmitry Ageev <dimichageev@icloud.com>
2020-04-20 17:26:04 +0300

    Продолжаю работу с гистограммой

--- js/start.js
+++ js/start.js
@@ -18,26 +18,26 @@ window.renderStatistics = function (ctx, names, times) {
   drawRect(dataCloud.startX, dataCloud.startY, dataCloud.widthRect, dataCloud.heightRect, dataCloud.colorRect[1]);
   writeText(dataCloud.text);
   drawHistogram(times, names);
-	
+
 	//Рисуем прямоугольник
 	function drawRect(axisX, axisY, width, height, fillColor) {
 		ctx.fillStyle = fillColor;
 		ctx.fillRect(axisX, axisY, width, height);
 	}
-	
+
 	//Выводим текст на облаке
 	function writetext(textArray) {
 		ctx.fillStyle = '#000';
 		ctx.font = '16px PT Mono';
-		
+
 		for (var i = 0; i < textArray.length; i++) {
 			ctx.fillText(textArray[i], dataCloud.startX + dataCloud.margin, dataCloud.startY + (i + 1) * 25);
 		}
 	}
-	
+
 	//Рисуем гистограмму
 	function drawHistogram(arryTimes, arrayNames) {
-		
+
 		var dataHistogram = {
 			barWidth: 40,
 			indent: 90,
@@ -46,7 +46,7 @@ window.renderStatistics = function (ctx, names, times) {
 			histogramHeight: 150,
 			paddingTop: 60
 		};
-		
-		
+
+		var step = dataHistogram.histogramHeight / (getMaxValue(times) - 0);
 	}
 };
