ab658282690304320ffa5fdde50d1f2ebd54f8d8
Dmitry Ageev <dimichageev@icloud.com>
2020-04-20 17:45:03 +0300

    Доделал работу с гистограммой

--- js/start.js
+++ js/start.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 window.renderStatistics = function (ctx, names, times) {
   var dataCloud = {
     startX: 100,
@@ -19,34 +18,49 @@ window.renderStatistics = function (ctx, names, times) {
   writeText(dataCloud.text);
   drawHistogram(times, names);
 
-	//Рисуем прямоугольник
-	function drawRect(axisX, axisY, width, height, fillColor) {
-		ctx.fillStyle = fillColor;
-		ctx.fillRect(axisX, axisY, width, height);
-	}
+  // Рисуем прямоугольник
+  function drawRect(axisX, axisY, width, height, fillColor) {
+    ctx.fillStyle = fillColor;
+    ctx.fillRect(axisX, axisY, width, height);
+  }
 
-	//Выводим текст на облаке
-	function writetext(textArray) {
-		ctx.fillStyle = '#000';
-		ctx.font = '16px PT Mono';
+  // Выводим надпись на облаке
+  function writeText(textArray) {
+    ctx.fillStyle = '#000';
+    ctx.font = '16px PT Mono';
 
-		for (var i = 0; i < textArray.length; i++) {
-			ctx.fillText(textArray[i], dataCloud.startX + dataCloud.margin, dataCloud.startY + (i + 1) * 25);
-		}
-	}
+    for (var i = 0; i < textArray.length; i++) {
+      ctx.fillText(textArray[i], dataCloud.startX + dataCloud.margin, dataCloud.startY + (i + 1) * 25);
+    }
+  }
 
-	//Рисуем гистограмму
-	function drawHistogram(arryTimes, arrayNames) {
+  // Рисуем гистограмму
+  function drawHistogram(arrayTimes, arrayNames) {
 
-		var dataHistogram = {
-			barWidth: 40,
-			indent: 90,
-			indentName: 20,
-			indentTime: 10,
-			histogramHeight: 150,
-			paddingTop: 60
-		};
+    var dataHistogram = {
+      barWidth: 40,
+      indent: 90,
+      indentName: 20,
+      indentTime: 10,
+      histogramHeight: 150,
+      paddingTop: 60,
+    };
 
-		var step = dataHistogram.histogramHeight / (getMaxValue(times) - 0);
-	}
+    var step = dataHistogram.histogramHeight / (getMaxValue(times) - 0);
+    var initialX = dataCloud.startX + dataCloud.margin;
+    var initialY = dataCloud.startY + dataHistogram.histogramHeight + dataHistogram.indentName + dataHistogram.indentTime + dataHistogram.paddingTop;
+
+    for (var i = 0; i < arrayTimes.length; i++) {
+      dataHistogram.barHeight = arrayTimes[i] * step;
+      var getY = initialY - arrayTimes[i] * step;
+      var getX = initialX + dataHistogram.indent * i;
+
+      ctx.fillStyle = fillBarColor(names[i]);
+      ctx.fillRect(getX, getY, dataHistogram.barWidth, dataHistogram.barHeight);
+
+      ctx.fillText(arrayNames[i], getX, initialY + dataHistogram.indentName);
+      ctx.fillText(arrayTimes[i].toFixed(0), getX, getY - dataHistogram.indentTime);
+    }
+  }
+
 };
